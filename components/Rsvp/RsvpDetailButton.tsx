17863a771e470817edca18e47cd1d463f3245165
artifacts-admin <dev@artifacts.art>
2025-08-20 16:53:01 +0900

    rsvp sheet vertical scroll

--- components/Rsvp/RsvpDetailButton.tsx
+++ components/Rsvp/RsvpDetailButton.tsx
@@ -17,6 +17,7 @@ import TimeSlotList from "./TimeSlotList";
 import AddTimeSlotButton from "./AddTimeSlotButton";
 import RsvpCloseBUtton from "./RsvpCloseButton";
 import NotifyAttendeesButton from "./NotifyAttendeesButton";
+import { ScrollArea } from "../ui/scroll-area";
 
 interface Props {
     rsvp: Rsvp
@@ -30,6 +31,7 @@ export default function RsvpDetailButton({ rsvp }: Props) {
     return (
         <Sheet>
             <SheetTrigger asChild><Button variant={"outline"} size={"sm"}>Detail</Button></SheetTrigger>
+
             <SheetContent className="w-[600px] max-w-none sm:max-w-none">
                 <SheetHeader>
                     <Badge>
@@ -39,80 +41,89 @@ export default function RsvpDetailButton({ rsvp }: Props) {
 
                 </SheetHeader>
 
-                <div className="p-4 space-y-4">
-
-                    <div className="flex gap-2">
-                        <RsvpEditButton rsvp={rsvp} />
-                        {rsvp.is_main == "1" &&
-                            <>
-                                <RsvpCloseBUtton rsvp={rsvp} />
-
-                                {rsvp.applicants?.length && rsvp.applicants?.length  > 0 &&
-                                    <NotifyAttendeesButton rsvp={rsvp} />
-                                }
-
-
-                            </>
-                        }
-
-
-                    </div>
 
 
 
-                    <hr />
 
-                    {rsvp.is_main == "1" ?
+                
+                <div className="overflow-y-auto p-4 space-y-4">
 
-                        // main RSVP
-                        <div className="space-y-4">
+                
 
-                            <Card>
-                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">
-                                        Capacity
-                                    </CardTitle>
-                                </CardHeader>
-                                <CardContent className="text-2xl">
-                                    {rsvp.applicants == null ? 0 : rsvp.count} / {rsvp.total_count}
-                                </CardContent>
-                            </Card>
+                        <div className="flex gap-2 ">
+                            <RsvpEditButton rsvp={rsvp} />
+                            {rsvp.is_main == "1" &&
+                                <>
+                                    <RsvpCloseBUtton rsvp={rsvp} />
 
-                            {rsvp.companion == "1" ? <Badge variant={"secondary"}>Companion is allowed.</Badge> : <Badge>Companion is not allowed.</Badge>}
+                                    {rsvp.applicants?.length && rsvp.applicants?.length > 0 &&
+                                        <NotifyAttendeesButton rsvp={rsvp} />
+                                    }
 
 
-
-                            {rsvp.applicants && rsvp.applicants.length > 0 ? <ApplicantList applicants={rsvp.applicants} /> : <p>No applicants yet.</p>}
-
-                        </div>
-
-                        :
-
-                        <div className="space-y-4">
-
-
-
-                            <div className="flex items-center justify-between">
-                                <h2 className="text-xl font-bold">Time Slot list</h2>
-                                <AddTimeSlotButton rsvp={rsvp} />
-                            </div>
-
-
-
-                            {rsvp.timeSlots != null &&
-                                <TimeSlotList rsvp={rsvp} />
+                                </>
                             }
 
 
-
-
                         </div>
 
-                    }
+
+
+                        <hr />
+
+                        {rsvp.is_main == "1" ?
+
+                            // main RSVP
+                            <div className="space-y-4 ">
+
+                                <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            Capacity
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent className="text-2xl">
+                                        {rsvp.applicants == null ? 0 : rsvp.count} / {rsvp.total_count}
+                                    </CardContent>
+                                </Card>
+
+                                {rsvp.companion == "1" ? <Badge variant={"secondary"}>Companion is allowed.</Badge> : <Badge>Companion is not allowed.</Badge>}
 
 
 
-                </div>
+                                {rsvp.applicants && rsvp.applicants.length > 0 ? <ApplicantList applicants={rsvp.applicants} /> : <p>No applicants yet.</p>}
+
+                            </div>
+
+                            :
+
+                            <div className="space-y-4 " >
+
+
+
+                                <div className="flex items-center justify-between">
+                                    <h2 className="text-xl font-bold">Time Slot list</h2>
+                                    <AddTimeSlotButton rsvp={rsvp} />
+                                </div>
+
+
+
+                                {rsvp.timeSlots != null &&
+                                    <TimeSlotList rsvp={rsvp} />
+                                }
+
+
+
+
+                            </div>
+
+                        }
+
+
+               </div>
+                
+
+
 
 
 
